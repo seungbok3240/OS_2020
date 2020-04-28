d75874e1b84fd6a07e12ad71b81b9964ea61fac4
seungbok3240 <seungbok3240@gmail.com>
2020-04-28 16:53:54 +0900

    OS_Prac06 ping-pong game

---
+++ client.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define FIFO_FILE	"./fifo_temp"
+#define BUF_SIZE	1337
+
+int main(void)
+{
+	int cnt = 0;
+	int fd;
+	int score = 100;
+	char buf[BUF_SIZE];
+	char pingstr[] = "pong\n";
+	sem_t* sem;
+
+	/*
+	 * To-do : initialize for game
+	 * get named semaphore, fifo, etc. 
+	 */
+	
+	fd = open(FIFO_FILE, O_RDWR);
+	
+	if ((sem = sem_open("serversema", O_CREAT, 0644, 0)) == SEM_FAILED)
+	{
+		perror("open");
+		exit(1);
+	}
+	
+	for (cnt=0; cnt<5; cnt++)
+	{
+		sem_wait(sem);
+		read(fd, buf, BUF_SIZE);
+		printf("[opponent] %s", buf);
+		printf("Your turn!\n");
+		memset(buf, 0x00, BUF_SIZE);
+		/*
+		 * To-do : complete game process
+		 */
+		fgets(buf, BUF_SIZE, stdin);
+		write(fd, buf, strlen(buf));
+
+		if (strcmp(buf, pingstr))
+		{
+			/*
+			 * To-do : handle wrong case
+			 */
+			printf("wrong! %d\n", -20);
+			score -= 20;
+		}
+		sem_post(sem);
+		sleep(1);
+	}
+
+	printf("Done! Your score : %d\n", score);
+
+	close (fd);
+
+	return 0;
+}
